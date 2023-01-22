90c6f9dcc855c21cd3105e02cbab4e4d49c50cd3
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2023-01-22 18:03:55 -0500

    Add comments to generate_data()

--- generate_data.R
+++ generate_data.R
@@ -1,22 +1,18 @@
-generate_AtemEtAl2017 <- function(n, censoring = "light", independent = TRUE, prophaz = TRUE) {
-  z <- rbinom(n = n, size = 1, prob = 0.5)
-  if (prophaz) {
-    x <- rweibull(n = n, shape = 0.75, scale = 0.25)  
-  } else {
-    x <- rweibull(n = n, shape = 0.6 + 0.9 * z, scale = 0.25)
-  }
-  e <- rnorm(n = n, mean = 0, sd = 1)
-  y <- 1 + 0.5 * x + 0.25 * z + e 
-  q <- ifelse(censoring == "light", 2, 0.35)
-  c1 <- rweibull(n = n, shape = 0.5, scale = q)
-  c2 <- rweibull(n = n, shape = 1, scale = q)
-  if (independent) {
-    c <- c2
-  } else {
-    c <- c1
-    c[y <= 1.1] <- c2[y <= 1.1]
-  }
-  w <- pmin(x, c)
-  d <- as.numeric(x <= c)
-  return(data.frame(z, w, y, d))
+# //////////////////////////////////////////////////////////////////////
+# Data generation function based on  ///////////////////////////////////
+# Atem et al. (2017)'s "independent censoring" setting  ////////////////
+# //////////////////////////////////////////////////////////////////////
+
+generate_AtemEtAl2017 = function(n, censoring = "light", independent = TRUE) {
+  z = rbinom(n = n, size = 1, prob = 0.5) # Uncensored covariate
+  x = rweibull(n = n, shape = 0.75, scale = 0.25)  # To-be-censored covariate
+  e = rnorm(n = n, mean = 0, sd = 1) # Random errors
+  y = 1 + 0.5 * x + 0.25 * z + e # Continuous outcome
+  q = ifelse(censoring == "light", 2, 
+             ifelse(censoring == "moderate", 0.35, 0.05)) # Rate parameter for censoring
+  c = rweibull(n = n, shape = 1, scale = q) # Random censoring mechanism
+  w <- pmin(x, c) # Observed covariate value
+  d <- as.numeric(x <= c) # "Event" indicator
+  dat = data.frame(z, w, y, d) # Construct dataset
+  return(dat)
 }
