9b57259ecffff4de6b22e8d382de6d13c8ab903f
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-01-27 18:57:25 -0500

    Delete NOTES from Table2_Parallel.R

--- Sim-Scripts/Table2_Parallel.R
+++ Sim-Scripts/Table2_Parallel.R
@@ -94,13 +94,3 @@ for (censoring in c("light", "heavy", "extra_heavy")) {
     }
   }
 }
-
-# //////////////////////////////////////////////////////////////////////
-# NOTES: When using the estimated survival function, we need to  ///////
-# to fit an imputation model or use an extrapolation method. This //////
-# makes the simulations for this table slower than Table 1. It took ~ 3/
-# minutes to run 1 replication per setting MacBook Pro (M1) with 16GB //
-# RAM. Based on this, it would take ~50 hours to run 1000 replications /
-# per setting. We parallelized instead, using sim_seed = 114-123 and ///
-# running reps = 100 replications per seed. ////////////////////////////
-# //////////////////////////////////////////////////////////////////////
