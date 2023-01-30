9e035ae25e8c4935d2a5c9361650c5dff0cd18f2
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-01-30 09:17:44 -0500

    Fix generate_data() in FigureS3_Parallel.R

--- Sim-Scripts/FigureS3_Parallel.R
+++ Sim-Scripts/FigureS3_Parallel.R
@@ -13,14 +13,14 @@ library(imputeCensRd) # To impute censored covariates
 ## This setting is new; it was created for this manuscript to consider a non-Weibull X
 generate_data = function(n, censoring = "light") {
   z = rbinom(n = n, size = 1, prob = 0.5) # Uncensored covariate
-  x = rweibull(n = n, shape = 0.75, scale = 0.25)  # To-be-censored covariate
+  x = rlnorm(n = n, meanlog = 0, sdlog = 0.5) # To-be-censored covariate
   e = rnorm(n = n, mean = 0, sd = 1) # Random errors
   y = 1 + 0.5 * x + 0.25 * z + e # Continuous outcome
   q = ifelse(test = censoring == "light", 
-             yes = 0.5, ## ~ 12%
+             yes = 0.2, # ~ 20% 
              no = ifelse(test = censoring == "heavy", 
-                         yes = 2.9, ## ~ 41%
-                         no = 20) ## ~ 78%
+                         yes = 0.4, # ~35%
+                         no = 1.67) # ~79%
   ) # Rate parameter for censoring
   c = rexp(n = n, rate = q) # Random censoring mechanism
   w = pmin(x, c) # Observed covariate value
