9ce7762f1c3f932d3fc8b1573ac2657346cc2444
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-02-22 15:17:06 -0500

    Rewrite sim scripts for Tables 2, S1--S2

--- Sim-Scripts/TableS2.R
+++ Sim-Scripts/TableS2.R
@@ -1,9 +1,8 @@
-# //////////////////////////////////////////////////////////////////////
-# Run simulation results for Table S1 //////////////////////////////////
-# Compare full cohort analysis to "gold standard" CMI based on true ////
-# function and adaptive quadrature vs. trapezoidal rule for Weibull X //
-# when X is independent of Z ///////////////////////////////////////////
-# //////////////////////////////////////////////////////////////////////
+# /////////////////////////////////////////////////////////////////////////
+# Run simulation results for Table S2 /////////////////////////////////////
+# Compare full cohort analysis to CMI based on estimated survival /////////
+# function and adaptive quadrature vs. trapezoidal rule for Log-Normal X //
+# /////////////////////////////////////////////////////////////////////////
 
 # Load packages
 ## Run once: install.packages("devtools")
@@ -13,14 +12,6 @@ library(imputeCensRd) # To impute censored covariates
 # Load data generating function generate_data() from GitHub 
 library(devtools) # To source an R script from GitHub
 source_url("https://raw.githubusercontent.com/sarahlotspeich/ItsIntegral/main/generate_data.R")
-
-# Write a function for the true survival function used to generate Weibull X 
-trueSURV = function(q, z) {
-  pweibull(q = q, 
-           shape = 0.75, 
-           scale = 0.25 + 0.25 * z,
-           lower.tail = FALSE)
-}
 
 # Set the number of replicates per setting
 reps = 1 ## We used a total of 1000, but see NOTES below
@@ -47,8 +38,8 @@ for (censoring in c("light", "heavy", "extra_heavy")) {
       # Generate data
       dat = generate_data(n = n, ## Sample size
                           censoring = censoring, ## Censoring setting
-                          distX = "weibull", ## Distribution for X
-                          XdepZ = FALSE) ## Since FALSE, assume that X is independent of Z
+                          distX = "lognormal", ## Distribution for X
+                          XdepZ = TRUE) ## Since TRUE, assume that X depends on Z
       
       # Save % censored
       sett_res$perc_censored[r] = 1 - mean(dat$d)
@@ -59,10 +50,11 @@ for (censoring in c("light", "heavy", "extra_heavy")) {
       
       # Method 2: CMI using adaptive quadrature 
       ## Create imputed dataset
-      imp_dat = cmi_custom(W = "w", Delta = "d", Z = "z", data = dat, 
-                           useSURV = trueSURV, trapezoidal_rule = FALSE)
+      imp_dat = cmi_sp(W = "w", Delta = "d", Z = "z", data = dat, 
+                       trapezoidal_rule = FALSE, ## approximate integral using adaptive quadrature
+                       surv_between = "cf", ## Breslow's estimator is carry-forward interpolated
+                       surv_beyond = "w") ## and extrapolated using the Weibull extension
       ## Check that imputation was successful 
-      ## (it always is when using trueSURV)
       if (imp_dat$code) {
         ## Fit model to imputed dataset
         fit_aq = lm(y ~ imp + z, data = imp_dat$imputed_data)
@@ -70,8 +62,10 @@ for (censoring in c("light", "heavy", "extra_heavy")) {
       }
       
       # Method 3: CMI using trapezoidal rule
-      imp_dat = cmi_custom(W = "w", Delta = "d", Z = "z", data = dat, 
-                           useSURV = trueSURV, trapezoidal_rule = TRUE)
+      imp_dat = cmi_sp(W = "w", Delta = "d", Z = "z", data = dat, 
+                       trapezoidal_rule = TRUE, ## approximate integral using adaptive quadrature
+                       surv_between = "cf", ## Breslow's estimator is carry-forward interpolated
+                       surv_beyond = "w") ## and extrapolated using the Weibull extension
       ## Check that imputation was successful 
       ## (it always is when using trueSURV)
       if (imp_dat$code) {
@@ -82,17 +76,18 @@ for (censoring in c("light", "heavy", "extra_heavy")) {
       
       # Save results
       write.csv(x = sett_res, 
-                file = paste0("TableS1_", censoring, "_n", n, "_seed", sim_seed, ".csv"), 
+                file = paste0("TableS2_", censoring, "_n", n, "_seed", sim_seed, ".csv"), 
                 row.names = F)
     }
   }
 }
 
-# //////////////////////////////////////////////////////////////////////
-# NOTES: When using the true survival function, there is no need  //////
-# to fit an imputation model or use an extrapolation method. This //////
-# makes the simulations for this table quicker than other tables. //////
-# It took <1 second to run 1 replication per setting MacBook Pro (M1) //
-# with 16GB RAM. Based on this, it would take ~11 minutes to run 1000 //
-# replications per setting. ////////////////////////////////////////////
-# //////////////////////////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////////////////////
+# NOTES: When using the estimated survival function, we need to  /////////
+# to fit an imputation model or use an extrapolation method. This ////////
+# makes the simulations for this table slower than Table 1. It took ~8 ///
+# minutes to run 1 replication per setting MacBook Pro (M1) with 16GB ////
+# RAM. Based on this, it would take ~127 hours to run 1000 replications //
+# per setting. We parallelized instead, using sim_seed = 114-133 and /////
+# running reps = 50 replications per seed. ///////////////////////////////
+# ////////////////////////////////////////////////////////////////////////
