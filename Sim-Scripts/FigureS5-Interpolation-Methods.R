568ae5feff9d624c0d7385c581f6efb88cd585b7
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2023-02-22 20:07:24 -0500

    Archive old versions and rename "the" keepers of Figure S5

--- Sim-Scripts/FigureS5-Interpolation-Methods.R
+++ Sim-Scripts/FigureS5-Interpolation-Methods.R
@@ -1,5 +1,5 @@
 # /////////////////////////////////////////////////////////////////////////
-# Run simulation results for Figure S4 ////////////////////////////////////
+# Run simulation results for Figure S5 ////////////////////////////////////
 # Compare CMI based on estimated survival function and adaptive ///////////
 # quadrature w/ different interpolation methods for Weibull X /////////////
 # /////////////////////////////////////////////////////////////////////////
@@ -9,26 +9,9 @@
 ## Run once: devtools::install_github("sarahlotspeich/imputeCensRd)
 library(imputeCensRd) # To impute censored covariates 
 
-# Data generation function based on Atem et al. (2017)'s "independent censoring" censoringing
-## We note that they generate data from a Weibull with shape = 1 and scale varied, 
-## But this is equivalent to an exponential (used here).
-generate_AtemEtAl2017 = function(n, censoring = "light") {
-  z = rbinom(n = n, size = 1, prob = 0.5) # Uncensored covariate
-  x = rweibull(n = n, shape = 0.75, scale = 0.25)  # To-be-censored covariate
-  e = rnorm(n = n, mean = 0, sd = 1) # Random errors
-  y = 1 + 0.5 * x + 0.25 * z + e # Continuous outcome
-  q = ifelse(test = censoring == "light", 
-             yes = 0.5, ## ~ 12%
-             no = ifelse(test = censoring == "heavy", 
-                         yes = 2.9, ## ~ 41%
-                         no = 20) ## ~ 78%
-  ) # Rate parameter for censoring
-  c = rexp(n = n, rate = q) # Random censoring mechanism
-  w = pmin(x, c) # Observed covariate value
-  d = as.numeric(x <= c) # "Event" indicator
-  dat = data.frame(x, z, w, y, d) # Construct data set
-  return(dat)
-}
+# Load data generating function generate_data() from GitHub 
+library(devtools) # To source an R script from GitHub
+source_url("https://raw.githubusercontent.com/sarahlotspeich/ItsIntegral/main/generate_data.R")
 
 # Set the number of replicates per setting
 reps = 1 ## We used a total of 1000, but see NOTES below
@@ -52,8 +35,10 @@ for (censoring in c("light", "heavy", "extra_heavy")) {
     # Loop over replicates 
     for (r in 1:reps) {
       # Generate data
-      dat = generate_AtemEtAl2017(n = n, 
-                                  censoring = censoring)
+      dat = generate_data(n = n, ## Sample size
+                          censoring = censoring, ## Censoring setting
+                          distX = "weibull", ## Distribution for X
+                          XdepZ = TRUE) ## Since TRUE, assume that X depends on Z
       
       # Save % censored
       sett_res$perc_censored[r] = 1 - mean(dat$d)
@@ -75,7 +60,7 @@ for (censoring in c("light", "heavy", "extra_heavy")) {
       
       # Save results
       write.csv(x = sett_res, 
-                file = paste0(censoring, "_n", n, "_seed", sim_seed, ".csv"), 
+                file = paste0("FigureS5_", censoring, "_n", n, "_seed", sim_seed, ".csv"), 
                 row.names = F)
     }
   }
