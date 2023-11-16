5e4ffb509e75c07b8842751f4f58693921ea95a0
Sarah Lotspeich (She/Her) <sarah.lotspeich@gmail.com>
2023-11-16 09:30:18 -0500

    Update FigureS5-Interpolation-Methods.R

--- Figure-Scripts/FigureS5-Interpolation-Methods.R
+++ Figure-Scripts/FigureS5-Interpolation-Methods.R
@@ -17,7 +17,7 @@ library(wesanderson) # For colors
 # //////////////////////////////////////////////////////////////////////
 
 # Read in simulation results 
-res = read.csv(file = "https://raw.githubusercontent.com/sarahlotspeich/ItsIntegral/main/Figure-Data/data_FigureS5.csv")
+res = read.csv(file = "https://raw.githubusercontent.com/sarahlotspeich/hybridCMI/main/Figure-Data/data_FigureS5.csv")
 ## Note: Simulations were run in parallel on random seeds 114-123 (with 100 reps per seed, per setting)
 ## This information is captured in the "sim" variable which is of the form seed-replicate. 
 
