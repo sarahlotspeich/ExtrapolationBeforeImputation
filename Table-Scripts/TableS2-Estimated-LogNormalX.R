49f08372836ddeba50adf9d85999541546e4c1cf
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-08-14 11:31:49 -0400

    Update file path in Table S2 (Rev) script (.R)

--- Table-Scripts/TableS2-Estimated-LogNormalX.R
+++ Table-Scripts/TableS2-Estimated-LogNormalX.R
@@ -2,7 +2,7 @@
 # Replicate Table S2 ///////////////////////////////////////////////////
 # Caption begins "Simulation results for log-normal $X$ from the full //
 # cohort analysis and imputation approaches using the estimated survival
-# function and adaptive quadrature versus the trapezoidal rule..." /////
+# function and Extrapolated CMI versus the trapezoidal rule..." /////
 # //////////////////////////////////////////////////////////////////////
 
 # Load packages
@@ -11,7 +11,7 @@ library(tidyr) # To gather wide tables
 library(kableExtra) # To format pretty tables
 
 # Read in simulation results 
-res = read.csv(file = "https://raw.githubusercontent.com/sarahlotspeich/ExtrapolationBeforeImputation/main/Table-Data/data_TableS2.csv")
+res = read.csv(file = "https://raw.githubusercontent.com/sarahlotspeich/ExtrapolationBeforeImputation/main/Table-Data/data_TableS2_rev.csv")
 ## Note: Simulations were run in parallel on random seeds 114-123 (with ~100 reps per seed, per setting)
 
 # Calculate average % censoring per censoring setting
@@ -23,16 +23,9 @@ res |>
 # Get convergence numbers for footnote /////////////////////////////////
 # //////////////////////////////////////////////////////////////////////
 res |> 
-  summarize(reps_na_aq = sum(is.na(beta_aq)),
-            reps_na_tr = sum(is.na(beta_tr))
-  ) ## Just 3 replicates out of 12,000 did not converge
-
-res |> 
-  group_by(censoring, n) |> 
-  summarize(reps_na_aq = sum(is.na(beta_aq)),
-            reps_na_tr = sum(is.na(beta_tr))
-  ) |> 
-  arrange(desc(reps_na_aq)) ## Converged in $\geq 99.8\%$ of replicates per setting
+  summarize(reps_na_extrap = sum(is.na(beta_extrap)),
+            reps_na_extrap_no = sum(is.na(beta_extrap_no))
+  ) ## No replicates out of 3,000 did not converge
 
 # //////////////////////////////////////////////////////////////////////
 # Summarize simulation results by setting //////////////////////////////
@@ -40,16 +33,13 @@ res |>
 
 # Unpivot results to get rows for each parameter (rather than columns)
 res_summ_long = res |> 
-  dplyr::select(-perc_censored) |> # use package prefix to avoid conflict with MASS::select
+  dplyr::select(-perc_censored, -B, -dplyr::starts_with(c("time", "se")), -rep) |> # use package prefix to avoid conflict with MASS::select
   gather(key = "param_calc", value = "est", -c(1:3)) |> 
   mutate(calc = gsub(pattern = ".*_", replacement = "", x = param_calc),
          calc = factor(x = calc, 
-                       levels = c("fc", "aq", "tr"),
-                       labels = c("Full Cohort", "Adaptive Quadrature", "Trapezoidal Rule")),
+                       levels = c("fc", "extrap", "no"),
+                       labels = c("Full Cohort", "Extrapolated CMI", "Non-Extrapolated CMI")),
          param = sub("_.*", "", param_calc),
-         censoring = factor(x = censoring,
-                            levels = c("light", "heavy", "extra_heavy"), 
-                            labels = c("Light", "Heavy", "Extra Heavy")),
          truth = ifelse(test = param == "alpha", 
                         yes = 1,
                         no = ifelse(test = param == "beta", 
@@ -71,13 +61,13 @@ res_summ_wide = res_summ_long |>
   pivot_wider(names_from = calc, 
               values_from = c("bias", "perc_bias", "se")) |> 
   arrange(param, censoring) |> 
-  mutate(`re_Adaptive Quadrature` = `se_Full Cohort` ^ 2 / `se_Adaptive Quadrature` ^ 2,
-         `re_Trapezoidal Rule` = `se_Full Cohort` ^ 2 / `se_Trapezoidal Rule` ^ 2,
+  mutate(`re_Extrapolated CMI` = `se_Full Cohort` ^ 2 / `se_Extrapolated CMI` ^ 2,
+         `re_Non-Extrapolated CMI` = `se_Full Cohort` ^ 2 / `se_Non-Extrapolated CMI` ^ 2,
          mid1 = "", mid2 = "", mid3 = "") |> 
   dplyr::select(censoring, n, param, mid1,
                 ends_with("Cohort"), mid2, 
-                ends_with("Quadrature"), mid3, 
-                ends_with("Rule")) 
+                ends_with("Extraplated CMI"), mid3, 
+                ends_with("Non-Extrapolated CMI")) 
 
 # //////////////////////////////////////////////////////////////////////
 # Format table for export to LaTex /////////////////////////////////////
@@ -92,8 +82,8 @@ format_num = function(num) {
 res_summ_wide |> 
   dplyr::select(-param) |> # delete param column - ordering is alpha, beta, gamma
   mutate_at(.vars = c("se_Full Cohort", 
-                      "se_Adaptive Quadrature", "re_Adaptive Quadrature", 
-                      "se_Trapezoidal Rule", "re_Trapezoidal Rule"), .funs = format_num) |>
+                      "se_Extrapolated CMI", "re_Extrapolated CMI", 
+                      "se_extrap_noapezoidal Rule", "re_extrap_noapezoidal Rule"), .funs = format_num) |>
   kable(format = "latex", booktabs = TRUE, escape = FALSE, 
         align = "llrcccccccrccccc") |> 
   kable_styling() 
