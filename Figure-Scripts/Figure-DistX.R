9c960cf43dbb6ee38a6e6aabd24d9f1f065f3dbb
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-09-04 21:26:43 -0400

    Commit Figure-DistX (.R, .png)

--- Figure-Scripts/Figure-DistX.R
+++ Figure-Scripts/Figure-DistX.R
@@ -14,10 +14,6 @@ ggplot() +
                 args = list(mean = 1.5, sd = 0.5), 
                 aes(color = "Normal"), 
                 xlim = c(0, 3))  + 
-  stat_function(fun = dt, 
-                args = list(df = 3), 
-                aes(color = "t"), 
-                xlim = c(0, 3)) + 
   stat_function(fun = dweibull, 
                 args = list(shape = 0.75, scale = 0.25), 
                 aes(color = "Weibull"), 
@@ -28,7 +24,8 @@ ggplot() +
                                 "Normal" = wes_palette("Zissou1", n = 5, type = "discrete")[3], 
                                 "t" = wes_palette("Zissou1", n = 5, type = "discrete")[4], 
                                 "Weibull" = wes_palette("Zissou1", n = 5, type = "discrete")[5])) + 
-  theme_minimal() + 
+  theme_minimal(base_size = 20) + theme(legend.position = "top") + 
   labs(x = "x", y = "Probability Density Function (PDF)")
 
-wes_palette("Zissou1", n = 5, type = "discrete")#[c(1,3,5)]
+# Save as 10" wide x 6" tall
+ggsave("Figure-DistX.png", width = 5, height = 4, units = "in")
