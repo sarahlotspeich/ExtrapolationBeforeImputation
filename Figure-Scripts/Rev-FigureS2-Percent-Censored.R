6eb1890ba0f09e1957e06607648d9a1ef239b683
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-09-09 17:06:23 -0400

    Remake Fig S2 (.R, .png)

---
+++ Figure-Scripts/Rev-FigureS2-Percent-Censored.R
@@ -0,0 +1,57 @@
+# //////////////////////////////////////////////////////////////////////
+# Replicate Figure S2 in Supplementary Materials  //////////////////////
+# Caption begins "We explored light (~17%), heavy (~49%), and extra ////
+# heavy (~82%) censoring in Weibull $X$, induced by generating ... /////
+# //////////////////////////////////////////////////////////////////////
+
+# Load packages
+library(dplyr) # For data wrangling
+library(tidyr) # To transform data
+library(ggplot2) # To create plots
+library(latex2exp) # To create LaTex labels for plots
+library(wesanderson) # For colors
+
+# //////////////////////////////////////////////////////////////////////
+# Read in simulation results from GitHub ///////////////////////////////
+# //////////////////////////////////////////////////////////////////////
+
+# Read in simulation results 
+res = read.csv(file = "https://raw.githubusercontent.com/sarahlotspeich/ExtrapolationBeforeImputation/main/Table-Data/data_Table1_rev.csv")
+res = read.csv("~/Documents/ExtrapolationBeforeImputation/Table-Data/data_Table1_rev.csv")
+
+# Calculate average % censoring per censoring setting
+res |> 
+  group_by(censoring) |> 
+  summarize(min_perc_censored = min(perc_censored),
+            avg_perc_censored = mean(perc_censored),
+            max_perc_censored = max(perc_censored))
+## Light 17% (7-29%)
+## Heavy 49% (33-63%)
+## Extra heavy 81% (66-94%)
+
+# //////////////////////////////////////////////////////////////////////
+# Create plot //////////////////////////////////////////////////////////
+# //////////////////////////////////////////////////////////////////////
+res |> 
+  mutate(censoring = factor(censoring, levels = c("light", "heavy", "extra_heavy"),
+                            labels = c("Light Censoring", 
+                                       "Heavy Censoring",
+                                       "Extra Heavy Censoring")),
+         n = factor(x = n, 
+                    levels = c(100, 500, 1000, 2000),
+                    labels = paste("n =", c(100, 500, 1000, 2000)))
+  ) |> 
+  ggplot(aes(x = n, y = perc_censored, fill = censoring)) + 
+  geom_boxplot(alpha = 0.7) + 
+  theme_minimal(base_size = 14) +
+  geom_hline(yintercept = 0.17, linetype = 2) + 
+  geom_hline(yintercept = 0.49, linetype = 2) + 
+  geom_hline(yintercept = 0.815, linetype = 2) + 
+  xlab("Sample Size") + 
+  ylab("Percent Censored Observations") + 
+  scale_fill_manual(values = wes_palette("Zissou1", n = 5, type = "discrete")[c(1,3,5)], name = "") +
+  theme(legend.position = "top") + 
+  scale_y_continuous(labels = scales::percent, breaks = c(0, 0.168, 0.484, 0.814, 1))
+
+# Save as 10" wide x 6" tall
+ggsave("Rev_FigureS2.png", width = 10, height = 6, units = "in")
